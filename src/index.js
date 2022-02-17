7c53db7bda2790352b5d612148b7f85412072a32
AquiAki <theorange.01@gmail.com>
2022-02-17 11:43:17 +0900

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>はじめまして</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>はじめまして</h1>
+    <p>私はreactを勉強します</p>
+  </div>,
+
+  document.getElementById("root")
+);
 
