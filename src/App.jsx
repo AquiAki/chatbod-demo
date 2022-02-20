639fe66946c1f1063b2f7d65faad85a10febb399
AquiAki <theorange.01@gmail.com>
2022-02-20 22:45:45 +0900

    Add the components of root, answer and chat. Customize material ui.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,87 @@
 import React from 'react';
 import defaultDataset from './dataset';
 import './assets/styles/style.css'
+import { AnswersList, Chats} from './components/index';
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      answers:[],
-      chats:[],
+    this.state = {
+      answers: [],
+      chats: [],
       currentId: "init",
       dataset: defaultDataset,
-      open: false
+      open: false,
+    };
+    this.selectAnswer = this.selectAnswer.bind(this);
+  }
+
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type:'question'
+    })
+    this.setState({
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId,
+    })
+  };
+
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch (true) {
+      case nextQuestionId === "init":
+        setTimeout(()=>this.displayNextQuestion(nextQuestionId), 500);
+        break;
+      // チェックした文字列を//で囲う。 *以前の文字列を判定。＾は行の先頭
+      case(/^https:*/.test(nextQuestionId)):
+        const a = document.createElement('a');
+        a.href = nextQuestionId;
+        // tragetを_blankでブラウザで別のタブで開く
+        a.target = '_blank';
+        a.click();
+        break;
+      default:
+        const chats = this.state.chats;
+        chats.push({
+          text: selectedAnswer,
+          type: "answer",
+        });
+
+        this.setState({
+          chats: chats,
+        });
+
+        setTimeout(()=> this.displayNextQuestion(nextQuestionId), 1000)
+        break;
+    }
+  };
+
+
+
+  // コンポーネントが初期化し最初のレンダーが終わった後に、何かしらの副作用のある処理をしたい時に使う
+  // 下のrender以下が走った後に、このcomponentDidMountが実行される。
+  // つまりinitのanswersが表示される
+  componentDidMount() {
+    const initAnswer = ""
+    this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate(){
+    const scrollArea = document.getElementById("scroll-area")
+    if (scrollArea){
+      scrollArea.scrollTop = scrollArea.scrollHeight
     }
   }
-  
+
+
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          {this.state.currentId}
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
         </div>
       </section>
     );
